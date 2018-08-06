c0e1b5513ca9cace008e46e380be209038a31cca
Ram Sundar <37891789+TechEntertaining@users.noreply.github.com>
2018-08-06 12:23:50 +0530

    Add files via upload

---
+++ src/javaconnect.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ram Sundar
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class javaconnect {
+    Connection conn=null;
+    
+  /*  public static Connection ConnecrDb(){
+        try{
+           Class.forName("org.sqlite.JDBC");
+           Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Ram Sundar\\Documents\\NetBeansProjects\\Banking_Management_System\\bank.sqlite");
+           return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }*/
+}
